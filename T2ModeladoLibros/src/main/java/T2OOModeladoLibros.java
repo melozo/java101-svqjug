ad8568dc9b8e7dc00fed4210cb5360a28ff0c91a
Melo <carmelo.zubeldia@gmail.com>
2017-10-15 23:34:18 +0200

    Corrección de errores en T2ModeladoLibros

--- T2ModeladoLibros/src/main/java/T2OOModeladoLibros.java
+++ T2ModeladoLibros/src/main/java/T2OOModeladoLibros.java
@@ -10,17 +10,15 @@ public class T2OOModeladoLibros {
         libro1.edadRecomendada = 10;
         libro1.numeroPaginas = 1000;
         libro1.abierto = false;
+        pepe.edad = 5;
 
         if (Libro.esAptoParapeques(pepe.edad, libro1.edadRecomendada)){
-            System.out.println("El libro " + libro1.titulo + " no es recomendable para peques");
-        } else {
             System.out.println("El libro " + libro1.titulo + " es recomendable" );
             System.out.println("Puedes abrir el libro. Debug->" + libro1.abrir());
-            if (libro1.abierto == true){ // esto no funciona
-                System.out.println("Bien!, ya tienes abierto el libro... comienza la lectura");
-            }
+//            if (libro1.abierto == true){ // esto no funciona
+//                System.out.println("Bien!, ya tienes abierto el libro... comienza la lectura");
+        } else {
+            System.out.println("El libro " + libro1.titulo + " no es recomendable para peques");
         }
-
-
     }
 }
